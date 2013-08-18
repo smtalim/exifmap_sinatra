2d3572e869807bfbb9e519fa8b584d6e9484e7b6
SatishTalim <satish@rubylearning.org>
2013-08-18 11:06:08 +0530

    commit

--- exifmap_sinatra.rb
+++ exifmap_sinatra.rb
@@ -7,7 +7,7 @@ not_found do
 end
 
 error do
-  erb :'500'
+  'Sorry there was a nasty error - ' + env['sinatra.error'].name
 end
 
 get '/' do
@@ -20,7 +20,6 @@ post '/show' do
     lat = photo.gps.latitude.to_s
     lng = photo.gps.longitude.to_s
     @photo_map = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&zoom=5&size=600x300&maptype=roadmap&center=#{lat},#{lng}&markers=color:blue%7Clabel:I%7C#{lat},#{lng}"
-    
     erb :show
   end
 end 
