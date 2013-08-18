30bd0efac7009ca99848581fa8802e032e2c0ee5
SatishTalim <satish@rubylearning.org>
2013-08-18 14:26:49 +0530

    commit

--- exifmap_sinatra.rb
+++ exifmap_sinatra.rb
@@ -20,7 +20,8 @@ post '/show' do
       photo = EXIFR::JPEG.new(params[:photo][:tempfile])
       lat = photo.gps.latitude.to_s
       lng = photo.gps.longitude.to_s
-      @photo_map = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&zoom=5&size=600x300&maptype=roadmap&center=#{lat},#{lng}&markers=color:blue%7Clabel:I%7C#{lat},#{lng}"
+      defaults = {:zoom => 5, :size => [600, 300], :maptype => 'roadmap'}
+      @photo_map = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&zoom=#{defaults[:zoom]}&size=#{defaults[:size][0]}x#{defaults[:size][1]}&maptype=#{defaults[:maptype]}&center=#{lat},#{lng}&markers=color:blue%7Clabel:I%7C#{lat},#{lng}"
       erb :show
     end
   rescue NoMethodError
